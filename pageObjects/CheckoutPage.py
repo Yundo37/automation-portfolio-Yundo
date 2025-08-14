73fa6674f1a58d8acece9c301944e00afbf45848
Yundo37 <nohoho37@gmail.com>
2025-08-14 21:27:31 +0900

    Create CheckoutPage.py

---
+++ pageObjects/CheckoutPage.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+
+from pageObjects.ConfirmPage import ConfirmPage
+
+class CheckOutPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    productTitle = (By.XPATH, "//div[@class='card h-100']")
+    productFooter = (By.XPATH, "div/button")
+    checkOut = (By.XPATH, "//button[@class='btn btn-success']")
+
+    def getProductTitles(self):
+        return self.driver.find_elements(*CheckOutPage.productTitle)
+
+    def getProductFooter(self, product):
+        return product.find_element(*CheckOutPage.productFooter)
+
+    def CheckOutItems(self):
+        self.driver.find_element(*CheckOutPage.checkOut).click()
+        confirmPage = ConfirmPage(self.driver)
+        return confirmPage
