2574460cd848457457fbd916c0d7f49a1590eab3
Yundo37 <nohoho37@gmail.com>
2025-08-14 21:26:22 +0900

    Create HomePageData.py

---
+++ testData/HomePageData.py
@@ -0,0 +1,3 @@
+class HomePageData:
+
+    test_Homepage_data = [{"username":"YB", "email":"nohoho37@gmail.com", "gender":"Male"}, {"username":"OB", "email":"porore37@naver.com", "gender":"Female"}, {"username":"FAIL_TEST", "email":"fail@test.com", "gender":"InvalidGender"}]
