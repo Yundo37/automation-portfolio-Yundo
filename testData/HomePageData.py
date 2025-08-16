531776913974215901b06ebf6d920187152f8786
Yundo37 <nohoho37@gmail.com>
2025-08-16 20:02:58 +0900

    Update HomePageData.py

--- testData/HomePageData.py
+++ testData/HomePageData.py
@@ -1,3 +1,8 @@
+# testData/HomePageData.py
 class HomePageData:
 
-    test_Homepage_data = [{"username":"YB", "email":"nohoho37@gmail.com", "gender":"Male"}, {"username":"OB", "email":"porore37@naver.com", "gender":"Female"}, {"username":"FAIL_TEST", "email":"fail@test.com", "gender":"InvalidGender"}]
+    test_Homepage_data = [
+        {"username": "YB", "email": "nohoho37@gmail.com", "gender": "Male"},
+        {"username": "OB", "email": "porore37@naver.com", "gender": "Female"},
+        {"username": "FAIL_TEST", "email": "fail@test.com", "gender": "Female"}  # ← 의도적 실패용 데이터
+    ]
