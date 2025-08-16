d4c0c3e0376afff1eae26f37d9ad5207f4f4ba2f
Yundo37 <nohoho37@gmail.com>
2025-08-16 22:15:40 +0900

    Update BaseClass.py

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -1,11 +1,10 @@
 import inspect
 import logging
-
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 
 
 @pytest.mark.usefixtures("setup")
@@ -17,14 +16,13 @@ class BaseClass:
         fileHandler = logging.FileHandler('logfile.log')
         formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
         fileHandler.setFormatter(formatter)
-
         logger.addHandler(fileHandler)
         logger.setLevel(logging.DEBUG)
         return logger
 
     def verifyLinkPresence(self, text):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.presence_of_element_located((By.LINK_TEXT, text)))
+        wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, text)))
 
     def selectOptionByText(self, element, text):
         sel = Select(element)
@@ -33,6 +31,3 @@ class BaseClass:
     def selectOptionByIndex(self, element, index):
         sel = Select(element)
         sel.select_by_index(index)
-
-    def wait_for_element_clickable(self, locator, timeout=10):
-        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
